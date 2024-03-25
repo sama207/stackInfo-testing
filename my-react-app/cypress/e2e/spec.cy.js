903bd7b6b3e90995ceae74a0889578989e4d2d42
sama <samashalabi2018@gmail.com>
2024-03-25 20:41:43 +0200

    c++ clicked checking done

--- my-react-app/cypress/e2e/spec.cy.js
+++ my-react-app/cypress/e2e/spec.cy.js
@@ -1,22 +1,30 @@
-describe('press show filter button ', () => {
-  it(' check if the button text will change', () => {
-    cy.visit('https://stacksinfo.web.app')
-    cy.get('.filter-button').should('contain','Show Filters')
-    cy.get('.filter-button').click()
-    cy.get('.filter-button').should('contain','Hide Filters')
-  })
-}) 
+// describe('press show filter button ', () => {
+//   it(' check if the button text will change', () => {
+//     cy.visit('https://stacksinfo.web.app')
+//     cy.get('.filter-button').should('contain','Show Filters')
+//     cy.get('.filter-button').click()
+//     cy.get('.filter-button').should('contain','Hide Filters')
+//   })
+// }) 
 describe('press backend then c++', () => {
   it('c++ checked', () => {
     cy.visit('https://stacksinfo.web.app')
+    cy.intercept('POST', '/searchAndFilter').as('getResponse');
+
     cy.get('.filter-button').click()
     cy.get('svg.MuiSvgIcon-root[data-testid="KeyboardArrowDownIcon"]').eq(0).click();
     cy.get('.filter-option').eq(0).get('[for="C++"]').click()
     cy.get('.filter-option').eq(0).get('.PrivateSwitchBase-input').should('have.attr', 'data-indeterminate', 'true');
-  //ضل نتاكد انو ال
-  //result length=2 (the companies that provide c++ is 2)  
+    // Assuming your application sends a response, you can intercept the response using cy.intercept
+
+    // Wait for the response to be received
+    cy.wait('@getResponse').then((interception) => {
+      // Access the response body and assert on the array length
+      const responseData = interception.response.body;
+      expect(responseData.results.length).to.be.equal(2); // Adjust this assertion as needed
+    });
   })
-}) 
+})
 describe('press backend -> c++ ->ASP.NET Core ', () => {
   it('c++ & ASP.NET Core checked', () => {
     cy.visit('https://stacksinfo.web.app')
@@ -27,13 +35,13 @@ describe('press backend -> c++ ->ASP.NET Core ', () => {
     cy.get('.filter-option').eq(1).get('[for="ASP .NET Core"]').click()
     cy.get('.filter-option').eq(1).get('.PrivateSwitchBase-input').should('have.attr', 'data-indeterminate', 'true');
   //ضل نتاكد انو ال
-  //result length=2 (the companies that provide c++ is 2)  
+  //result length=2 (the companies that provide c++ is 2)
   })
 })
-// describe('press backend btn', () => {
-//   it('display all companies or businesses that use alteast one backend stack', () => {
-//     cy.visit('https://stacksinfo.web.app')
-//     cy.get('.filter-button').click()
-//     cy.get('.dropdown-title').eq(0).get('')
-//   })
-// }) 
+// // describe('press backend btn', () => {
+// //   it('display all companies or businesses that use alteast one backend stack', () => {
+// //     cy.visit('https://stacksinfo.web.app')
+// //     cy.get('.filter-button').click()
+// //     cy.get('.dropdown-title').eq(0).get('')
+// //   })
+// // }) 
