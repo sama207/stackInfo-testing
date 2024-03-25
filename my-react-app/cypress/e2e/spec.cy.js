9da2baa8f490e2eac571b0019af160c9cd66538f
sama <samashalabi2018@gmail.com>
2024-03-25 14:03:02 +0200

    show/hide filter testing & click on backend -> c++ test

--- my-react-app/cypress/e2e/spec.cy.js
+++ my-react-app/cypress/e2e/spec.cy.js
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('press show filter button ', () => {
+  it(' check if the button text will change', () => {
     cy.visit('https://stacksinfo.web.app')
+    cy.get('.filter-button').should('contain','Show Filters')
+    cy.get('.filter-button').click()
+    cy.get('.filter-button').should('contain','Hide Filters')
   })
-})
+  
+}) 
+describe('press backend then c++', () => {
+  it('c++ checked', () => {
+    cy.visit('https://stacksinfo.web.app')
+    cy.get('.filter-button').click()
+    cy.get('svg.MuiSvgIcon-root[data-testid="KeyboardArrowDownIcon"]').eq(0).click();
+    cy.get('.filter-option').eq(0).get('[for="C++"]').click()
+       
+  })
+  
+}) 
