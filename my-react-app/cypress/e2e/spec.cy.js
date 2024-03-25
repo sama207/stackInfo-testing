c20aba88b8dadc145b85e1902c0e567ff1ea24bc
sama <samashalabi2018@gmail.com>
2024-03-25 14:34:12 +0200

    some changes

--- my-react-app/cypress/e2e/spec.cy.js
+++ my-react-app/cypress/e2e/spec.cy.js
@@ -5,7 +5,6 @@ describe('press show filter button ', () => {
     cy.get('.filter-button').click()
     cy.get('.filter-button').should('contain','Hide Filters')
   })
-  
 }) 
 describe('press backend then c++', () => {
   it('c++ checked', () => {
@@ -13,7 +12,27 @@ describe('press backend then c++', () => {
     cy.get('.filter-button').click()
     cy.get('svg.MuiSvgIcon-root[data-testid="KeyboardArrowDownIcon"]').eq(0).click();
     cy.get('.filter-option').eq(0).get('[for="C++"]').click()
-       
+    cy.get('.filter-option').eq(0).get('.PrivateSwitchBase-input').should('have.attr', 'data-indeterminate', 'true');
+  //ضل نتاكد انو ال
+  //result length=2 
   })
-  
 }) 
+describe('press backend -> c++ ->ASP.NET Core ', () => {
+  it('c++ & ASP.NET Core checked', () => {
+    cy.visit('https://stacksinfo.web.app')
+    cy.get('.filter-button').click()
+    cy.get('svg.MuiSvgIcon-root[data-testid="KeyboardArrowDownIcon"]').eq(0).click();
+    cy.get('.filter-option').eq(0).get('[for="C++"]').click()
+    cy.get('.filter-option').eq(0).get('.PrivateSwitchBase-input').should('have.attr', 'data-indeterminate', 'true');
+    cy.get('.filter-option').eq(1).get('[for="ASP .NET Core"]').click()
+    cy.get('.filter-option').eq(1).get('.PrivateSwitchBase-input').should('have.attr', 'data-indeterminate', 'true');
+
+  })
+})
+// describe('press backend btn', () => {
+//   it('display all companies or businesses that use alteast one backend stack', () => {
+//     cy.visit('https://stacksinfo.web.app')
+//     cy.get('.filter-button').click()
+//     cy.get('.dropdown-title').eq(0).get('')
+//   })
+// }) 
