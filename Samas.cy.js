d8aa9deb63b91e27f3c789654dc40ed2c3488f5b
Rama Hasiba <rama.rhasiba@gmail.com>
2024-03-26 06:15:45 +0300

    Update Samas.cy.js

--- Samas.cy.js
+++ Samas.cy.js
@@ -1,14 +1,2 @@
-describe("Testing AND Logic", () => {
-  beforeEach(() => {
-    cy.visit("https://stacksinfo.web.app");
-    cy.get(".filter-button").click(); 
-    cy.get('svg.MuiSvgIcon-root[data-testid="KeyboardArrowDownIcon"]').each(($tech, index)=>{
-        if (!([2, 5, 7, 8].includes(index))) {
-            cy.get($tech).click();
 
-        }
-    })
-  });
-
-});
  
